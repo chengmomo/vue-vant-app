f5238750f98d68f590f1a1f709ef5c875ee65033
13956452721 <1373537294@qq.com>
2019-05-09 14:46:58 +0800

    补充提交

--- src/view/recommend/index.vue
+++ src/view/recommend/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="recommend">
-    recommend
+    <van-list>
+      <van-cell
+      v-for="item in list"
+      :key="item"
+      :title="item"
+      @click="goRoute(item)"/>
+    </van-list>
   </div>
 </template>
 
@@ -9,11 +15,31 @@ export default {
   name: 'recommend',
   props: {
 
+  },
+  data() {
+    return {
+      list: ['1','上班','午休','下班','休息日'],
+      loading: true,
+      finished: false
+    };
+  },
+  methods: {
+      goRoute(item) {
+          console.log(item)
+          this.$router.push({
+              name: 'strategy'
+          })
+      }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+  .van-cell {
+    margin: 10px;
+    width: 355px;
+    height: 160px;
+    padding: 0;
+  }
 </style>
