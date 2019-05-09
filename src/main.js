f14ad1a1745fe0835a267c4412328310b1ee6382
15951810818 <hap.gaojing@163.com>
2019-05-09 11:06:50 +0800

    引入vant

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import Router from 'vue-router'
 import router from './router'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
 
 Vue.config.productionTip = false
 
-Vue.use(Router)
+Vue.use(Router).use(Vant)
 
 new Vue({
 	router,
